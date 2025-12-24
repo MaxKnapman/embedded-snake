0c6a79ec3a07b564e7b89acaa9d9cf17404efe24
MaxKnapman <max.knapman@hotmail.com>
2025-12-24 16:50:30 +0000

    Update app.ino
    Fruit spawns
    Segments are 4x4 pixels
    Removed redundant function

--- app.ino
+++ app.ino
@@ -1,66 +1,77 @@
+#include <stdlib.h>
 #include <U8g2lib.h>
+#include <time.h>
 
 U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0);
 
-int xCoord = 0;
-int yCoord = 0;
-int up = 2;
-int down = 3;
-int left = 4;
-int right = 5;
-int direction = 4;
-long movementInterval = 500;
 long previousMillis = 0;
+char direction = 'r'; //r l u d character values for direction
+int ycoord = 1;
+int xcoord = 1;
+int length = 0;
+int ysegments[10];
+int xsegments[10];
+int fruitx;
+int fruity;
 
-void changeDirection(int newDirection) {
-  direction = newDirection;
-  delay(100);
+void move(char direction) {
+  switch (direction) {
+    case 'l':
+      xcoord -= 1;
+      break;
+    case 'r':
+      xcoord += 1;
+      break;
+    case 'u':
+      ycoord -= 1;
+      break;
+    case 'd':
+      ycoord += 1;
+      break;
+  }
+  if (xcoord == fruitx && ycoord == fruity) {
+    length++;
+    fruit();
+  }
+}
+
+void fruit() {
+  fruitx = rand() % 32;
+  fruity = rand() % 16;
 }
 
 void setup() {
+  srand(time(NULL));
   u8g2.begin();
-  pinMode(up, INPUT);
-  pinMode(down, INPUT);
-  pinMode(left, INPUT);
-  pinMode(right, INPUT);
+  pinMode(2, INPUT_PULLUP); //up, down, right, left
+  pinMode(3, INPUT_PULLUP);
+  pinMode(4, INPUT_PULLUP);
+  pinMode(5, INPUT_PULLUP);
+  fruit();
 }
 
 void loop() {
-
+  u8g2.clearBuffer();
   long currentMillis = millis();
 
-  // get button inputs
-  if (digitalRead(up) == LOW && direction != 2) {
-    changeDirection(1);
+  if (digitalRead(2) == LOW && direction != 'd') {
+    direction = 'u';
   }
-  if (digitalRead(down) == LOW && direction != 1) {
-    changeDirection(2);
+  if (digitalRead(3) == LOW && direction != 'u') {
+    direction = 'd';
   }
-  if (digitalRead(left) == LOW && direction != 4) {
-    changeDirection(3);
+  if (digitalRead(4) == LOW && direction != 'l') {
+    direction = 'r';
   }
-  if (digitalRead(right) == LOW && direction != 3) {
-    changeDirection(4);
+  if (digitalRead(5) == LOW && direction != 'r') {
+    direction = 'l';
   }
 
-  //movement
-  if (currentMillis - previousMillis >= movementInterval) {
+  if (currentMillis - previousMillis >= 500) {
     previousMillis = currentMillis;
-    switch(direction) {
-      case 1:
-        yCoord--;
-        break;
-      case 2:
-        yCoord++;
-        break;
-      case 3:
-        xCoord--;
-        break;
-      case 4:
-        xCoord++;
-        break;
-    }
-    u8g2.drawPixel(xCoord, yCoord);
+    move(direction);
+    u8g2.drawBox((xcoord * 4), (ycoord * 4), 4, 4);
+    u8g2.drawBox((fruitx * 4), (fruity * 4), 4, 4);
     u8g2.sendBuffer();
   }
 }
