bd53bcd178027be72c36423f6a41188b592d1570
MaxKnapman <max.knapman@hotmail.com>
2025-12-27 16:10:32 +0000

    Update app.ino
    Fixed fruit spawning inside the snake

--- app.ino
+++ app.ino
@@ -7,18 +7,18 @@ U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0);
 long previousMillis = 0;
 char direction = 'r'; //r l u d character values for direction
 int length = 1;
-int y_segments[10] {1}; //max length 10 for now
-int x_segments[10] {1};
+int y_segments[20] {1}; //max length based on available ram
+int x_segments[20] {1};
 int fruit_x_coord;
 int fruit_y_coord;
 
 void move(char direction) {
   if (x_segments[0] == fruit_x_coord && y_segments[0] == fruit_y_coord) { //increase length
     length++;
-    fruit();
+    spawn_fruit();
   }
 
-  for (int i = 9; i > 0; i--) { //shunt segments to the right
+  for (int i = length; i > 0; i--) { //shunt segments to the right
     x_segments[i] = x_segments[i - 1];
     y_segments[i] = y_segments[i - 1];
   }
@@ -39,9 +39,14 @@ void move(char direction) {
   }
 }
 
-void fruit() { //randomly spawn fruit
-  fruit_x_coord = rand() % 32;
-  fruit_y_coord = rand() % 16;
+void spawn_fruit() { //randomly spawn fruit
+  fruit_x_coord = rand() % 16;
+  fruit_y_coord = rand() % 8;
+  for (int i = 0; i < length; i++) { // recursion to stop fruit from spawning inside the snake
+    if (fruit_x_coord == x_segments[i] && fruit_y_coord == y_segments[i]) {
+      spawn_fruit();
+    }
+  }
 }
 
 void setup() {
@@ -51,7 +56,7 @@ void setup() {
   pinMode(3, INPUT_PULLUP);
   pinMode(4, INPUT_PULLUP);
   pinMode(5, INPUT_PULLUP);
-  fruit();
+  spawn_fruit();
 }
 
 void loop() {
@@ -75,9 +80,9 @@ void loop() {
     previousMillis = currentMillis;
     move(direction);
     for (int i = 0; i < length; i++) {
-      u8g2.drawBox((x_segments[i] * 4), (y_segments[i] * 4), 4, 4);
+      u8g2.drawBox((x_segments[i] * 8), (y_segments[i] * 8), 8, 8);
     }
-    u8g2.drawBox((fruit_x_coord * 4), (fruit_y_coord * 4), 4, 4);
+    u8g2.drawBox((fruit_x_coord * 8), (fruit_y_coord * 8), 8, 8);
     u8g2.sendBuffer();
   }
 }
